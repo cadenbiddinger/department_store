c5829616916a45ad794865f4c6e25e8b0792abdd
Caden Biddinger <caden@Cadens-MacBook-Air.local>
2020-05-20 15:15:10 -0600

    added model and controller

--- db/migrate/20200520210958_create_departments.rb
+++ db/migrate/20200520210958_create_departments.rb
@@ -1,10 +1,7 @@
 class CreateDepartments < ActiveRecord::Migration[6.0]
   def change
     create_table :departments do |t|
-      t.string :index
-      t.string :show
-      t.string :new
-      t.string :edit
+      t.string :name
 
       t.timestamps
     end
--- db/migrate/20200520211138_create_store_items.rb
+++ db/migrate/20200520211138_create_store_items.rb
@@ -1,10 +1,7 @@
 class CreateStoreItems < ActiveRecord::Migration[6.0]
   def change
     create_table :store_items do |t|
-      t.string :index
-      t.string :show
-      t.string :new
-      t.string :edit
+      t.string :name
 
       t.timestamps
     end
