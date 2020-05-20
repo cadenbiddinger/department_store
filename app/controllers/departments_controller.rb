3d559f809a3f454bf2b883312b2f3301e89b08e4
Caden Biddinger <caden@Cadens-MacBook-Air.local>
2020-05-20 15:38:59 -0600

    update

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,13 +1,32 @@
 class DepartmentsController < ApplicationController
+
   def index
+    @departments = Departments.all
   end
 
   def show
+    @department = Department.find(params[:id])
   end
 
   def new
+    @department = Department.new
   end
 
-  def edit
+  def create
+    @department = Department.new(departments_params)
+    if @department.save
+      redirect_to 
   end
+
+
+  def edit
+    @department = Department.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root "departments#index"
+
+  resources :departments do
+    resources :store_items
+  end
+
+  resources :store_items do
+    resources :comments
+  end
 end
